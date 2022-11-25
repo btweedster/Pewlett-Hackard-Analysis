1039b6328997731b09135a917287f5b38318233b
btweedster <benjamin@Bens-Mac.local>
2022-11-25 00:39:39 -0500

    schema updated to correct primary keys in 'titles' and 'dept_emp' tables.

--- schema.sql
+++ schema.sql
@@ -42,14 +42,15 @@ CREATE TABLE dept_emp (
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no,dept_no)
 );
 
+-- emp_no, from_date used for now
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no,from_date)
 );
